ed078997d709b34012e9e755ff57ac053c3d04c2
Juan Carlos López <jcarloslopezl@hotmail.com>
2017-12-09 23:33:51 -0600

    Adds the Mapbox API token to environments file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiamNsb3BleiIsImEiOiJjamIwYjlteDI2eXQ0MzFucWp4MW9vb3pzIn0.6OuVAnCI1Zo1SIFW6huHcg'
+  }
 };
