008b96a8a4c0fe84d96a596cf2f666803dbeb17b
Juan Carlos López <jcarloslopezl@hotmail.com>
2017-12-10 22:51:27 -0600

    Adds references to firebase and mapbox apiKeys for testing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,15 @@ export const environment = {
   production: false,
   mapbox: {
     accessToken: 'pk.eyJ1IjoiamNsb3BleiIsImEiOiJjamIwYjlteDI2eXQ0MzFucWp4MW9vb3pzIn0.6OuVAnCI1Zo1SIFW6huHcg'
+  },
+  firebase: {
+    apiKey: "AIzaSyAD69-o9J9IGZ_S9Aguxq88CQyVX1e0qCA",
+    authDomain: "mapbox-99de3.firebaseapp.com",
+    databaseURL: "https://mapbox-99de3.firebaseio.com",
+    projectId: "mapbox-99de3",
+    storageBucket: "mapbox-99de3.appspot.com",
+    messagingSenderId: "302912623567"
   }
 };
+
+
