a3362d0d03e9cbe1c70c8263d058c31777d2edad
Juan Carlos López <jcarloslopezl@hotmail.com>
2018-01-18 17:55:36 -0600

    Get items in constructor and fix removeMarker method using itemsRef list

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -1,30 +1,35 @@
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 import { GeoJson } from './../map';
 import * as mapboxgl from 'mapbox-gl';
 
 @Injectable()
 export class MapService {
+  itemsRef: AngularFireList<any>;
+  items: Observable<any[]>;
 
   constructor(private db: AngularFireDatabase) { 
     mapboxgl.accessToken = environment.mapbox.accessToken;
-  }
 
-  getMarkers() {
-    return this.db.list('markers').snapshotChanges().map(actions => {      
-      return actions.map(action => ({ key: action.key, ...action.payload.val() }));
+    this.itemsRef = db.list('markers');
+    this.items = this.itemsRef.snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
     });
   }
 
+  getMarkers() {
+    return this.items;
+  }
+
   createMarker(data: GeoJson) {
-    return this.db.list('/markers').push(data);
+    return this.itemsRef.push(data);
   }
 
   removeMarker($key: string) {
-    console.log('Remove: ', $key);
-    return this.db.object('/markers' + $key).remove();
+    return this.itemsRef.remove($key);
   }
 
 }
